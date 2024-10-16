8d7d7f171cd66a920c30db7ccf3480a1148ced6a
yaozhikai <75172744+yaozhikai@users.noreply.github.com>
2024-10-16 21:30:12 +1300

    Add files via upload

---
+++ TS- v4.R
@@ -0,0 +1,116 @@
+options(scipen = 999)
+
+library(ggplot2)
+library(dplyr)
+
+
+ts_df <- read.csv('Timespan.csv')
+
+colnames(ts_df)[1] <- "Year Month"
+colnames(ts_df) <- ts_df[1, ]
+ts_df <- ts_df[-1, ]
+
+ts_df_total <- ts_df[, -c(2:64)]
+
+colnames(ts_df_total)[1] <- "Year_Month"
+
+ts_df_total$`Year_Month` <- as.character(ts_df_total$`Year_Month`)
+
+ts_df_total$Year <- as.numeric(substr(ts_df_total$`Year_Month`, 1, 4))
+ts_df_total$Month <- as.numeric(substr(ts_df_total$`Year_Month`, 6, 7))
+
+ts_df_total$`Year Month` <- as.Date(paste(ts_df_total$Year, ts_df_total$Month, "01", sep = "-"), format = "%Y-%m-%d")
+
+ts_df_total$Visitor <- as.numeric(ts_df_total$Visitor)
+ts_df_total$TOTAL <- as.numeric(ts_df_total$TOTAL)
+
+#update- exclude nz and au passport from analysis
+ts_df_total$Residence <- as.numeric(ts_df_total$Residence)
+ts_df_total$Student <- as.numeric(ts_df_total$Student)
+ts_df_total$Work <- as.numeric(ts_df_total$Work)
+ts_df_total$Other <- as.numeric(ts_df_total$Other)
+ts_df_total$TOTALnoNZAU <- ts_df_total$Residence + ts_df_total$Student + ts_df_total$Visitor + ts_df_total$Work + ts_df_total$Other
+
+
+start_year <- 2003
+start_month <- 7
+ts_visitor <- ts(ts_df_total$Visitor, start = c(start_year, start_month), frequency = 12)
+ts_total <- ts(ts_df_total$TOTALnoNZAU, start = c(start_year, start_month), frequency = 12)
+
+# ts plot
+plot(ts_visitor, col = "blue", ylim = range(c(ts_visitor, ts_total)),
+     ylab = "Number of Visitors", xlab = "Year", main = "Time Series of Foreign Visitors to New Zealand",
+     xaxt = "n", yaxt = "n")
+
+axis(2, at = pretty(range(c(ts_visitor, ts_total))),
+     labels = format(pretty(range(c(ts_visitor, ts_total))), big.mark = ","), las = 1)
+
+years <- seq(start_year, 2024, by = 1)
+axis(1, at = years, labels = years)
+
+lines(ts_total, col = "red")
+legend("topright", legend = c("Visitors Visa", "Total Visitors"),
+       col = c("blue", "red"), lty = 1)
+
+#recovery percentage of visitor visa
+
+ts_df_total$Visitor <- as.numeric(ts_df_total$Visitor)
+annual_visitors <- aggregate(Visitor ~ Year, data = ts_df_total, sum)
+annual_visitors_sorted <- annual_visitors[order(-annual_visitors$Visitor), ]
+print(annual_visitors_sorted)
+
+visitors_2018 <- annual_visitors[annual_visitors$Year == 2018, "Visitor"]
+visitors_2023 <- annual_visitors[annual_visitors$Year == 2023, "Visitor"]
+recovery_rate <- (visitors_2023 / visitors_2018) * 100
+recovery_rate
+
+# lm on season (whole time span)
+get_season <- function(month) {
+  if (month %in% c(12, 1, 2)) {
+    return("Summer")
+  } else if (month %in% c(3, 4, 5)) {
+    return("Autumn")
+  } else if (month %in% c(6, 7, 8)) {
+    return("Winter")
+  } else {
+    return("Spring")
+  }
+}
+
+ts_df_total$Season <- sapply(ts_df_total$Month, get_season)
+
+ts_df_total$Season <- factor(ts_df_total$Season, levels = c("Summer", "Autumn", "Winter", "Spring"))
+
+visitor_lm <- lm(Visitor ~ Season, data = ts_df_total)
+
+summary(visitor_lm)
+
+ggplot(ts_df_total, aes(x = Season, y = Visitor)) +
+  geom_boxplot(fill = "lightblue") + 
+  geom_point(position = position_jitter(width = 0.1), alpha = 0.5) +  
+  geom_smooth(method = "lm", se = FALSE, col = "red") + 
+  labs(title = "Visitors vs. Season in New Zealand",
+       x = "Season",
+       y = "Number of Visitors") +
+  theme_minimal()
+
+
+##lm prior covid
+ts_df_filtered <- subset(ts_df_total, `Year Month` <= as.Date("2020-03-01"))
+ts_df_filtered$Season <- sapply(ts_df_filtered$Month, get_season)
+
+
+ts_df_filtered$Season <- factor(ts_df_filtered$Season, levels = c("Summer", "Autumn", "Winter", "Spring"))
+
+visitor_lm_filtered <- lm(Visitor ~ Season, data = ts_df_filtered)
+
+summary(visitor_lm_filtered)
+
+ggplot(ts_df_filtered, aes(x = Season, y = Visitor)) +
+  geom_boxplot(fill = "lightblue") +  
+  geom_point(position = position_jitter(width = 0.1), alpha = 0.5) +  
+  geom_smooth(method = "lm", se = FALSE, col = "red") +  
+  labs(title = "Visitors vs. Season in New Zealand (Before March 2020)",
+       x = "Season",
+       y = "Number of Visitors") +
+  theme_minimal()
